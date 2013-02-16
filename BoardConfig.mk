5f03a178e464ba1c11f656faa8d804d0b00d248c
Matt Filetto <matt.filetto@gmail.com>
2013-02-16 01:25:08 -0800

    update kernel config
    Change-Id: I08932db826001ff060e2354361d915a6bcf45c02

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -36,6 +36,12 @@ BOARD_KERNEL_PAGE_SIZE := 2048
 BOARD_KERNEL_CMDLINE := console=ttyHSL0 androidboot.hardware=pyramid no_console_suspend=1
 TARGET_KERNEL_CONFIG := pyramid_defconfig
 
+# Kernel [Build]
+TARGET_KERNEL_CONFIG := ChronicKernel_defconfig
+TARGET_KERNEL_SOURCE := kernel/htc/msm8660
+TARGET_KERNEL_CUSTOM_TOOLCHAIN := linaro
+BUILD_KERNEL := true
+
 # GPS
 BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := pyramid
 
